f64ba5821efb946239506512fc4e7b1fb28547af
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2023-08-25 18:21:52 +0530

    added office validation that have 3 sub forms types

--- propertyValidation/spreader.js
+++ propertyValidation/spreader.js
@@ -1,32 +1,54 @@
 // Custom Modules
-const { flat_apartment } = require("../propertyValidation/flat_apartment");
-const { independentHouse_villa } = require("../propertyValidation/independentHouse_villa");
-const { independent_builderFloor } = require("../propertyValidation/independent_builderFloor");
-const { servicedApartment } = require("../propertyValidation/servicedApartment");
-const { rk_studio } = require("../propertyValidation/rk_studio");
-const { farmhouse } = require("../propertyValidation/farmhouse");
-const { plot_land } = require("../propertyValidation/plot_land");
+const { flat_apartment } = require("./Sell_Residential/flat_apartment");
+const { independentHouse_villa } = require("./Sell_Residential/independentHouse_villa");
+const { independent_builderFloor } = require("./Sell_Residential/independent_builderFloor");
+const { servicedApartment } = require("./Sell_Residential/servicedApartment");
+const { rk_studio } = require("./Sell_Residential/rk_studio");
+const { farmhouse } = require("./Sell_Residential/farmhouse");
+const { plot_land } = require("./Sell_Residential/plot_land");
+
+const { readyToMoveSpace } = require("./Sell_Commercial/Office/readyToMoveSpace");
+const { bareShellOfficeSpace } = require("./Sell_Commercial/Office/bareShellOfficeSpace");
+const { coworkingOfficeSpace } = require("./Sell_Commercial/Office/coworkingOfficeSpace");
 
 
 // Function to send payload to dedicated property validators
-function spreader(payload) { 
+function spreader(payload) {
     if (payload.lookingFor == "Sell") {
-        if (payload.propertyType == "Flat / Apartment") {
-            return flat_apartment(payload);
-        } else if (payload.propertyType == "Independent House / Villa") {
-            return independentHouse_villa(payload);
-        } else if (payload.propertyType == "Independent / Builder Floor") {
-            return independent_builderFloor(payload);
-        } else if (payload.propertyType == "Serviced Apartment") {
-            return servicedApartment(payload);
-        } else if (payload.propertyType == "1RK / Studio Apartment") {
-            return rk_studio(payload);
-        } else if (payload.propertyType == "Farmhouse") {
-            return farmhouse(payload);
-        } else if (payload.propertyType == "Plot / Land") {
-            return plot_land(payload);
+        if (payload.propertyGroup == "Residential") {
+            if (payload.propertyType == "Flat / Apartment") {
+                return flat_apartment(payload);
+            } else if (payload.propertyType == "Independent House / Villa") {
+                return independentHouse_villa(payload);
+            } else if (payload.propertyType == "Independent / Builder Floor") {
+                return independent_builderFloor(payload);
+            } else if (payload.propertyType == "Serviced Apartment") {
+                return servicedApartment(payload);
+            } else if (payload.propertyType == "1RK / Studio Apartment") {
+                return rk_studio(payload);
+            } else if (payload.propertyType == "Farmhouse") {
+                return farmhouse(payload);
+            } else if (payload.propertyType == "Plot / Land") {
+                return plot_land(payload);
+            } else {
+                return { "msg": "ERROR", "error": `${payload.propertyType} is Wrong Property Type` }
+            }
+        } else if (payload.propertyGroup == "Commercial") {
+            if (payload.propertyType == "Office") {
+                if (payload.officeType == "Ready to move office space") {
+                    return readyToMoveSpace(payload);
+                } else if (payload.officeType == "Bare shell office space") {
+                    return bareShellOfficeSpace(payload);
+                } else if (payload.officeType == "Co-working office space") {
+                    return coworkingOfficeSpace(payload);
+                } else {
+                    return { "msg": "ERROR", "error": `${payload.officeType} is Wrong Office Type` }
+                }
+            } else {
+                return { "msg": "ERROR", "error": `Data Validation Not Implemented for ${payload.officeType}` }
+            }
         } else {
-            return { "msg": "ERROR", "error": `${payload.propertyType} is Wrong Property Type` }
+            return { "msg": "ERROR", "error": `${payload.propertyGroup} is Wrong Property Group` }
         }
     } else if (payload.lookingFor == "Rent") {
         return { "msg": "ERROR", "error": `Data Validation Not Implemented for ${payload.lookingFor}` }
