d02bca0b559ed535909fe9b5a2b3f3c67b65e095
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2023-10-05 15:50:02 +0530

    updated pre leased rented

--- propertyValidation/Sell_Commercial/Industry/manufacturing.js
+++ propertyValidation/Sell_Commercial/Industry/manufacturing.js
@@ -260,7 +260,7 @@ function manufacturing(data) {
 
         obj.preLeased_RentedDetails = preLeased_RentedDetails;
     } else {
-        if (Object.keys(obj.preLeased_RentedDetails).length) {
+        if (Object.keys(data.preLeased_RentedDetails).length) {
             obj.preLeased_RentedDetails = {};
         }
     }
